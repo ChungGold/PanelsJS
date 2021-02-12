442aa50caa2ff73ba577b7713ce460c1a50d56fa
ChungGold <74089047+ChungGold@users.noreply.github.com>
2021-02-12 12:53:32 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,68 @@
 const container = document.getElementById('img-container');
+let a = 0;
 
-for (let i = 1; i <= 97; i++) {
-    if (i <= 12) {
+for (let i = 1; i <= 193; i++) {
+    a+= 0.015;
+
+    if (i <= 16) {
         container.innerHTML +=
-        `<div class='panels'>
-            <img src="birds.jpg" style='object-position: calc(${(i-1) * -159}px) top;'>
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-1) * -120}px) top;'>
         </div>`
-    } else if (i >= 13 && i <= 24) { 
+    } else if (i >= 17 && i <= 32) { 
         container.innerHTML +=
-        `<div class='panels'>
-            <img src="birds.jpg" style='object-position: calc(${(i-13) * -159}px) -117px;'>
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-17) * -120}px) -78px;'>
         </div>`
-    } else if (i >= 25 && i <= 36) { 
+    } else if (i >= 33 && i <= 48) { 
         container.innerHTML +=
-        `<div class='panels'>
-            <img src="birds.jpg" style='object-position: calc(${(i-25) * -159}px) -234px;'>
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-33) * -120}px) -156px;'>
         </div>`
-    } else if (i >= 37 && i <= 48) { 
+    } else if (i >= 49 && i <= 64) { 
         container.innerHTML +=
-        `<div class='panels'>
-            <img src="birds.jpg" style='object-position: calc(${(i-37) * -159}px) -351px;'>
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-49) * -120}px) -234px;'>
         </div>`
-    } else if (i >= 49 && i <= 60) { 
+    } else if (i >= 65 && i <= 80) { 
         container.innerHTML +=
-        `<div class='panels'>
-            <img src="birds.jpg" style='object-position: calc(${(i-49) * -159}px) -468px;'>
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-65) * -120}px) -312px;'>
         </div>`
-    } else if (i >= 61 && i <= 72) { 
+    } else if (i >= 81 && i <= 96) { 
         container.innerHTML +=
-        `<div class='panels'>
-            <img src="birds.jpg" style='object-position: calc(${(i-61) * -159}px) -585px;'>
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-81) * -120}px) -390px;'>
         </div>`
-    } else if (i >= 73 && i <= 84) { 
+    } else if (i >= 97 && i <= 112) { 
         container.innerHTML +=
-        `<div class='panels'>
-            <img src="birds.jpg" style='object-position: calc(${(i-73) * -159}px) -702px;'>
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-97) * -120}px) -469px;'>
         </div>`
-    } else if (i >= 85 && i <= 96) { 
+    } else if (i >= 113 && i <= 128) { 
         container.innerHTML +=
-        `<div class='panels'>
-            <img src="birds.jpg" style='object-position: calc(${(i-85) * -159}px) -819px;'>
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-113) * -120}px) -546px;'>
+        </div>`
+    } else if (i >= 129 && i <= 144) { 
+        container.innerHTML +=
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-129) * -120}px) -624px;'>
+        </div>`
+    } else if (i >= 145 && i <= 160) { 
+        container.innerHTML +=
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-145) * -120}px) -702px;'>
+        </div>`
+    } else if (i >= 161 && i <= 176) { 
+        container.innerHTML +=
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-161) * -120}px) -780px;'>
+        </div>`
+    } else if (i >= 177 && i <= 192) { 
+        container.innerHTML +=
+        `<div class='panels' style='animation-delay: ${a+2.5}s;'>
+            <img src="trees.jpg" style='object-position: calc(${(i-177) * -120}px) -858px;'>
         </div>`
     } 
-    
-    console.log(container);
 }
